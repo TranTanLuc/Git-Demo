1b84aeb4f266aab457c27766b952c7852b0d5ab2
Tran Tan Luc <luc972000@gmail.com>
2023-03-07 23:16:02 +0700

    commit project as

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Git Demo");
         System.out.println("Git test");
+        System.out.println("Hello");
+        System.out.println("Tran tan Luc");
     }
 }
